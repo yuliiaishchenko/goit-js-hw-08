7df581605cbed4769534240d00c07a4e41228001
yuliiaishchenko <romashka.ukr@gmail.com>
2023-03-12 15:29:48 +0200

    1308

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,46 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
+import { galleryItems } from './gallery-items.js';
+
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const divRef = document.querySelector(".gallery");
+
+function createGalleryMarkup (items) {
+    return items.map(({preview, original, description}) => {
+        return `<div class = "gallery__item">
+    <a class = "gallery__link" href = "${original}">
+    <img class = "gallery__image" 
+    src = "${preview}"
+    data-source = "${original}"
+    alt = "${description}"/>
+    </a>
+    </div>`}).join("");
+};
+
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+divRef.innerHTML = addGalleryMarkup;
+
+// divRef.addEventListener("click", onImageClick);
+
+// function onImageClick (e) {n
+//     blockStandardAction(e);
+//     if (e.target.nodeName !== "IMG") { return;}
+
+
+// //     const instance = basicLightbox .create (`
+// //     <img src = "${e.target.dataset.source}" width = "800" height = "600">`);
+// //     instance.show();
+
+// // divRef.addEventListener("keydown", (e) => {if (e.code === 'Escape'){
+// //     instance.close();
+// // }});
+
+// }
+
+function blockStandardAction (e) {
+    e.preventDefault();
+}
